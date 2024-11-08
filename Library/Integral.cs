ad810517dff1acb1ebccacd4f228a16c8b07fc97
Soloma <Soloma3040450@Gmail.com>
2024-11-08 16:06:27 +0300

    final coomit

---
+++ Library/Integral.cs
@@ -0,0 +1,93 @@
+﻿namespace Library
+{
+    /// <summary>
+    /// Представляет определенный интеграл функции.
+    /// </summary>
+    public class Integral
+    {
+        private string _funcName;
+        private Func<double, double> _function;
+        private double _lowerLimit;
+        private double _upperLimit;
+        private double? _computedValue;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса.
+        /// </summary>
+        /// <param name="function">Функция, которую нужно интегрировать.</param>
+        /// <param name="lowerLimit">Нижний предел интегрирования.</param>
+        /// <param name="upperLimit">Верхний предел интегрирования.</param>
+        /// <param name="funcName">Название функции.</param>
+        public Integral(Func<double, double> function, double lowerLimit, double upperLimit, string funcName = "unknown function")
+        {
+            _funcName = funcName;
+            _function = function;
+            _lowerLimit = lowerLimit;
+            _upperLimit = upperLimit;
+            _computedValue = null;
+        }
+
+        /// <summary>
+        /// Получает название функции.
+        /// </summary>
+        /// <returns>Название функции.</returns>
+        public string GetName()
+        {
+            return _funcName;
+        }
+
+        /// <summary>
+        /// Вычисляет значение определенного интеграла.
+        /// </summary>
+        /// <returns>Значение интеграла или <c>null</c>, если интеграл не вычислен.</returns>
+        public double? Calculate()
+        {
+            double stepSize = (_upperLimit - _lowerLimit) / 1000;
+            double sum = 0.0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                double x = _lowerLimit + (i + 0.5) * stepSize;
+                sum += _function(x);
+            }
+            _computedValue = sum * stepSize;
+            return _computedValue;
+        }
+
+        /// <summary>
+        /// Оператор сложения для интегралов.
+        /// </summary>
+        /// <param name="a">Первый интеграл.</param>
+        /// <param name="b">Второй интеграл.</param>
+        /// <returns>Сумма значений интегралов.</returns>
+        public static double? operator +(Integral a, Integral b)
+        {
+            return a._computedValue + b._computedValue;
+        }
+
+        /// <summary>
+        /// Оператор умножения для интеграла и числа.
+        /// </summary>
+        /// <param name="integral">Интеграл.</param>
+        /// <param name="number">Число.</param>
+        /// <returns>Результат умножения значения интеграла на число.</returns>
+        public static double? operator *(Integral integral, double number)
+        {
+            return integral._computedValue * number;
+        }
+
+        /// <summary>
+        /// Выводит информацию о функции и ее значении на консоль.
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Функция: {_funcName}");
+            Console.WriteLine($"Нижний предел: {_lowerLimit}");
+            Console.WriteLine($"Верхний предел: {_upperLimit}");
+            if (_computedValue != null)
+            {
+                Console.WriteLine($"Значение: {_computedValue}\n");
+            }
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,149 @@
+﻿using Library;
+using NCalc;
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+        string name;
+        int[] userChoice = new int[2];
+        int count;
+        double upperLimit;
+        double lowerLimit;
+        double number;
+        Func<double, double> func;
+        double? result;
+        List<Integral> integrals = new List<Integral>();
+        Integral integralA = new Integral(x => 1 / (1 + x * x), 2, 7, "1 / (1 + x^2)");
+        Integral integralB = new Integral(x => x * x, 2, 7, "x^2");
+        integralA.Calculate();
+        integralB.Calculate();
+        integrals.Add(integralA);
+        integrals.Add(integralB);
+        bool endIsNear = false;
+        while (!endIsNear)
+        {
+            Console.WriteLine("1. Задать интеграл");
+            Console.WriteLine("2. Параметры интегралов");
+            Console.WriteLine("3. Суммировать интегралы");
+            Console.WriteLine("4. Умножение интеграла");
+            Console.WriteLine("5. выход");
+            input = Console.ReadLine();
+            if (input != null)
+            {
+                userChoice[0] = int.Parse(input);
+                switch (userChoice[0])
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Укажите формулу интеграла: ");
+                            input = Console.ReadLine();
+                            func = (x) =>
+                            {
+                                var expr = new Expression(input);
+                                expr.Parameters["x"] = x;
+                                return (double)expr.Evaluate();
+                            };
+                            Console.WriteLine("Укажите нижний предел: ");
+                            lowerLimit = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Укажите верхний предел: ");
+                            upperLimit = Convert.ToDouble(Console.ReadLine());
+                            if(upperLimit < lowerLimit)
+                            {
+                                Console.WriteLine("Error. Wrong data, try again.");
+                                break;
+                            }
+                            Console.WriteLine("Название функции: ");
+                            name = Console.ReadLine();
+                            integrals.Add(new Integral(func, lowerLimit, upperLimit, name));
+                            integrals[integrals.Count-1].Calculate();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Выберите интеграл из списка:");
+                            for (count = 1; count <= integrals.Count; count++)
+                            {
+                                Console.WriteLine($"{count} - {integrals[count-1].GetName()}");
+                            }
+                            userChoice[0] = int.Parse(Console.ReadLine());
+                            integrals[userChoice[0]-1].Print();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Выберите интеграл из списка:");
+                            for (count = 1; count <= integrals.Count; count++)
+                            {
+                                Console.WriteLine($"{count} - {integrals[count-1].GetName()}");
+                            }
+                            userChoice[0] = int.Parse(Console.ReadLine());
+                            if(userChoice[0] > integrals.Count)
+                            {
+                                Console.WriteLine("Error. Wrong input, try again.");
+                                break;
+                            }
+
+                            Console.WriteLine("Выберите второй интеграл:");
+                            for (count = 1; count <= integrals.Count; count++)
+                            {
+                                Console.WriteLine($"{count} - {integrals[count-1].GetName()}");
+                            }
+                            userChoice[1] = int.Parse(Console.ReadLine());
+                            if(userChoice[1] > integrals.Count)
+                            {
+                                Console.WriteLine("Error. Wrong input, try again.");
+                                break;
+                            }
+                            result = integrals[userChoice[0]-1] + integrals[userChoice[1]-1];
+                            Console.WriteLine($"Сумма интегралов равна {result}");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Выберите интеграл из списка:");
+                            for (count = 1; count <= integrals.Count; count++)
+                            {
+                                Console.WriteLine($"{count} - {integrals[count-1].GetName()}");
+                            }
+                            input = Console.ReadLine();
+                            userChoice[0] = int.Parse(input);
+                            if(userChoice[0] > integrals.Count)
+                            {
+                                Console.WriteLine("Error. Wrong input, try again.");
+                                break;
+                            }
+
+                            Console.WriteLine("Введите множитель: ");
+                            input = Console.ReadLine();
+                            if (double.TryParse(input, out number))
+                            {
+                                result = integrals[userChoice[0]-1] * number;
+                                Console.WriteLine($"Произведение интеграла и {userChoice[1]} равна {result}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error. wrong input, try again.");
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            endIsNear = true;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Error. Wrong input, try again.\n");
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error. Null input, try again.\n");
+            }
+        }
+    }
+}
