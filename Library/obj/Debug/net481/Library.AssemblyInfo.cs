10b8e754ea95ec9b364608329f02696931f921ad
Soloma <Soloma3040450@Gmail.com>
2024-11-22 14:32:15 +0300

    OOP lab work-3 Cool!

--- Library/obj/Debug/net481/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net481/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74675f5b4c98128f8f294b1813a5d1cd10f20daf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc30b99a9fb5064247814658ba4bd0bde4e8f871")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
